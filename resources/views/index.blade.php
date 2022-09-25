3c0fd56eef845844ed4c7041b7663bade1b37042
Joss <jossokta@gmail.com>
2022-09-25 23:11:28 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+
+<html>
+     <head>
+        <title>Home</title>
+
+        <link rel="stylesheet" href="/css/style.css" />
+     </head>
+
+
+<body>
+    <div id="doc2" class="jos-t7">
+        <div id="inner">
+
+            <div id="hd">
+                <div class="jos-gc">
+                    <div class="jos-u first">
+                        <h1>Josua Oktavianus S</h1>
+                        <h2>Local Staff</h2>
+                    </div>
+
+                    <div class="jos-u">
+                        <div class="contact-info">
+                            <h3><a href="mailto:josokta@gmail.com">josokta@gmail.com</a></h3>
+                            <h3>+6281223414672</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="bd">
+                <div id="jos-main">
+                    <div class="jos-b">
+
+                        <div class="jos-gf">
+                            <div class="jos-u first">
+                                <h2>Profile</h2>
+                            </div>
+                            <div class="jos-u">
+                                <p class="enlarge">
+                                    Work wholeheartedly for customer satisfaction. Strive to develop self-ability to support corporate and personal interests.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="jos-gf">
+                            <div class="jos-u first">
+                                <h2>Skills</h2>
+                            </div>
+                            <div class="jos-u">
+
+                                    <div class="talent">
+                                        <h2>Communications</h2>
+                                        <p>Able to communicate well.	</p>
+                                    </div>
+
+                                    <div class="talent">
+                                        <h2>Adapt</h2>
+                                        <p>Easy to adapt to new environment.	 </p>
+                                    </div>
+
+                                    <div class="talent">
+                                        <h2>Computerized</h2>
+                                        <p>Able to master computer programs proficiently.</p>
+                                    </div>
+                            </div>
+                        </div>
+
+                        <div class="jos-gf">
+                            <div class="jos-u first">
+                                <h2>Technical</h2>
+                            </div>
+                            <div class="jos-u">
+                                <ul class="talent">
+                                    <li>Java</li>
+                                    <li>HTML</li>
+                                    <li class="last">PHP</li>
+                                </ul>
+
+                                <ul class="talent">
+                                    <li>Excel</li>
+                                    <li>Power Point</li>
+                                    <li class="last">Word</li>
+                                </ul>
+
+                                <ul class="talent">
+                                    <li>Laravel</li>
+                                    <li class="last">CSS</li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+            <div id="ft">
+                <p>Josua Oktavianus Situmorang &mdash; <a href="mailto:josokta@gmail.com">josokta@gmail.com</a> &mdash; +6281223414672</p>
+            </div>
+
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
